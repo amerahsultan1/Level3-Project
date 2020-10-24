87082304dffeae0d5646c601c937c5de2167673c
Ubuntu <ubuntu@ip-10-0-1-103.me-south-1.compute.internal>
2020-10-24 11:16:46 +0000

    update Dockerfile

--- e2e-js-test/runner.sh
+++ e2e-js-test/runner.sh
@@ -6,12 +6,12 @@ files=$testfile
 
 if [ -z $testfile ]
 then
-  files=$(ls ./*_test.js)
+  files=$(ls /e2e-test/*_test.js)
 fi
 
 for test in $files
 do
-  node ./test_helper.js
+  node /e2e-test/test_helper.js
   casperjs test $test
   ret=$?
   if [ ! $ret == "0" ]; then code=1; fi
